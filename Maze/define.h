4489a857eefc944f8e70cdda9adfee062e4b0a01
Yelz <yscbb2011@gmail.com>
2020-01-04 22:58:17 +0000

    Draw rectangle function (printwall)

--- Maze/define.h
+++ Maze/define.h
@@ -5,3 +5,19 @@
 #define WINDOW_SIZE_WIDTH 600
 #define WINDOW_SIZE_HEIGHT 600
 
+struct Vertex {
+	GLfloat x;
+	GLfloat y;
+	GLfloat z;
+};
+
+struct Quad4 {
+	Vertex vertexes[4];
+};
+
+struct Wall {
+	GLfloat x;
+	GLfloat y;
+	GLfloat z;
+	GLfloat size;
+};
--- Maze/main.cpp
+++ Maze/main.cpp
@@ -5,6 +5,47 @@
 using namespace cv;
 #include "define.h"
 
+void xyzToVertex(Vertex& vertex, float x, float y, float z) {
+	vertex.x = x;
+	vertex.y = y;
+	vertex.z = z;
+}
+
+void vertexToVertex(Vertex& det, Vertex src) {
+	det.x = src.x;
+	det.y = src.y;
+	det.z = src.z;
+}
+
+void vertexesToQuad(Quad4& quad, Vertex vertex0, Vertex vertex1, Vertex vertex2, Vertex vertex3) {
+	vertexToVertex(quad.vertexes[0], vertex0);
+	vertexToVertex(quad.vertexes[1], vertex1);
+	vertexToVertex(quad.vertexes[2], vertex2);
+	vertexToVertex(quad.vertexes[3], vertex3);
+}
+
+void printWall(Wall wall){
+	//The 8 vertexs of a rectangle(wall)
+	Vertex vertexes[8];
+	xyzToVertex(vertexes[0], wall.x, wall.y, wall.z);
+	xyzToVertex(vertexes[1], wall.x + wall.size, wall.y, wall.z);
+	xyzToVertex(vertexes[2], wall.x + wall.size, wall.y + wall.size, wall.z);
+	xyzToVertex(vertexes[3], wall.x, wall.y + wall.size, wall.z);
+	xyzToVertex(vertexes[4], wall.x, wall.y, wall.z + wall.size);
+	xyzToVertex(vertexes[5], wall.x + wall.size, wall.y, wall.z + wall.size);
+	xyzToVertex(vertexes[6], wall.x + wall.size, wall.y + wall.size, wall.z + wall.size);
+	xyzToVertex(vertexes[7], wall.x, wall.y + wall.size, wall.z + wall.size);
+
+	//The surface of the rectangle(wall)
+	Quad4 quads[6];
+	vertexesToQuad(quads[0], vertexes[0], vertexes[1], vertexes[2], vertexes[3]);
+	vertexesToQuad(quads[1], vertexes[0], vertexes[1], vertexes[5], vertexes[4]);
+	vertexesToQuad(quads[2], vertexes[2], vertexes[3], vertexes[7], vertexes[6]);
+	vertexesToQuad(quads[3], vertexes[1], vertexes[2], vertexes[6], vertexes[5]);
+	vertexesToQuad(quads[4], vertexes[0], vertexes[3], vertexes[7], vertexes[4]);
+	vertexesToQuad(quads[5], vertexes[4], vertexes[5], vertexes[6], vertexes[7]);
+}
+
 void init() {
 
 }
