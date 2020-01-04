6852662efbdac0cc566f49bc8588cd1eec980665
Yelz <yscbb2011@gmail.com>
2020-01-04 22:12:41 +0000

    Setting up opencv2

---
+++ Maze/main.cpp
@@ -0,0 +1,14 @@
+#include <GL\glut.h>
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+using namespace cv;
+
+void init() {
+
+}
+
+int main() {
+
+
+}
